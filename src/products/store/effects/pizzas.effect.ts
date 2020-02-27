959bba50db5700fb4a48b47148574c7f3ca04fb3
Vincent Bamps <vincent.bamps@fednot.be>
2020-02-27 12:21:42 +0100

    vba-effects : commented code

--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -10,17 +10,21 @@ import * as fromServices from '../../services';
 @Injectable()
 export class PizzasEffects {
   constructor(
+    // actions$ is an Observable
     private actions$: Actions,
     private pizzaService: fromServices.PizzasService
   ) {}
 
   @Effect()
+    // listening on Action.LOAD_PIZZAS.
+    // when LOAD_PIZZAS call the service and return a SUCCESS action with pizzas[] as payload
   loadPizzas$ = this.actions$.ofType(pizzaActions.LOAD_PIZZAS).pipe(
     switchMap(() => {
       return this.pizzaService
         .getPizzas()
         .pipe(
           map(pizzas => new pizzaActions.LoadPizzasSuccess(pizzas)),
+          // the of operator return this as an Observable
           catchError(error => of(new pizzaActions.LoadPizzasFail(error)))
         );
     })
